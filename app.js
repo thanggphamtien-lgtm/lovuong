57683ae3dfd43d9b4524e38c6937593d607d3628
thanggphamtien-lgtm <thanggphamtien@gmail.com>
2026-02-20 18:43:29 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,49 +1,10 @@
-const rotateNotice = document.getElementById("rotateNotice");
+// app.js (BẢN SẠCH - KHÔNG fullscreen, KHÔNG orientation)
+
 const startBtn = document.getElementById("startBtn");
 
-function isPortrait(){
-  return window.innerHeight > window.innerWidth;
-}
-
-function isMobile(){
-  return /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-}
-
-function checkOrientation(){
-  if(isPortrait() && isMobile()){
-    rotateNotice.style.display = "flex";
-    startBtn.style.display = "none";
-  } else {
-    rotateNotice.style.display = "none";
-    startBtn.style.display = "block";
-  }
-}
-
-checkOrientation();
-window.addEventListener("resize", checkOrientation);
-window.addEventListener("orientationchange", checkOrientation);
-
-startBtn.onclick = async function(){
-
-  if(isMobile()){
-    // MOBILE: fullscreen trước
-    if(document.documentElement.requestFullscreen){
-      try{
-        await document.documentElement.requestFullscreen();
-      }catch(e){}
-    }
-
-    if(screen.orientation && screen.orientation.lock){
-      screen.orientation.lock("landscape").catch(()=>{});
-    }
-
-    // đợi 300ms rồi mới chuyển trang
-    setTimeout(()=>{
-      window.location.href = "intro.html";
-    },300);
-
-  } else {
-    // PC: KHÔNG fullscreen, chuyển trang luôn
+if(startBtn){
+  startBtn.onclick = function(){
+    // chỉ chuyển sang intro.html
     window.location.href = "intro.html";
   }
-};
+}
