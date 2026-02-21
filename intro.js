95a33a092eebf9e2acb51761ab3f2741a0d35ba2
thanggphamtien-lgtm <thanggphamtien@gmail.com>
2026-02-21 16:10:15 +0700

    Add files via upload

--- intro.js
+++ intro.js
@@ -1,67 +1,87 @@
-// intro.js (BẢN ĐÃ FIX)
+var buttonActive = document.querySelector('.button button')
+buttonActive.onclick = function(){
+    var boxFlower = document.querySelector('.flower-img:nth-child(1)')
+    var boxFlower2 = document.querySelector('.flower-img:nth-child(2)')
+    var boxFlower3 = document.querySelector('.flower-img:nth-child(3)')
+    var boxFlower4 = document.querySelector('.flower-img:nth-child(4)')
+    var boxFlower5 = document.querySelector('.flower-img:nth-child(5)')
+    var boxFlower6 = document.querySelector('.flower-img:nth-child(6)')
+    var circleActive = document.querySelector('.circle')
+    var boxsliderimg1 = document.querySelector('.box-slider_img1')
+    var catActive = document.querySelector('.cat')
+    var numberActive = document.querySelector('.box-number')
+    var buttonDisplay = document.querySelector('.button')
+    var rhombus1 = document.querySelector('.rhombus:nth-child(1)')
+    var rhombus2 = document.querySelector('.rhombus:nth-child(2)')
+    var rhombusImg = document.querySelector('.rhombus-img')
+    var mailActive = document.querySelector('.mail')
 
-var buttonActive = document.querySelector('.button button');
-var mainContent = document.getElementById("mainContent");
+    boxFlower.classList.toggle("active")
+    boxFlower2.classList.toggle("active")
+    boxFlower3.classList.toggle("active")
+    boxFlower4.classList.toggle("active")
+    boxFlower5.classList.toggle("active")
+    boxFlower6.classList.toggle("active")
+    circleActive.classList.toggle("active")
+    boxsliderimg1.classList.toggle("active")
+    catActive.classList.toggle("active")
+    numberActive.classList.toggle("active")
+    buttonDisplay.classList.toggle("active")
+    rhombus1.classList.toggle("active")
+    rhombus2.classList.toggle("active")
+    rhombusImg.classList.toggle("active")
+    mailActive.classList.toggle("active")
+}
 
-function startAnimation(){
+var mail = document.querySelector(".mail")
+var slider3 = document.querySelector(".slider3")
+var closeSlider3 = document.querySelector(".fa-xmark")
+mail.onclick = function(){
+    slider3.classList.add("active")
+}
+closeSlider3.addEventListener('click', function(){
+    slider3.classList.remove('active')
+})
 
-  var boxFlower = document.querySelector('.flower-img:nth-child(1)');
-  var boxFlower2 = document.querySelector('.flower-img:nth-child(2)');
-  var boxFlower3 = document.querySelector('.flower-img:nth-child(3)');
-  var boxFlower4 = document.querySelector('.flower-img:nth-child(4)');
-  var boxFlower5 = document.querySelector('.flower-img:nth-child(5)');
-  var boxFlower6 = document.querySelector('.flower-img:nth-child(6)');
-  var circleActive = document.querySelector('.circle');
-  var boxsliderimg1 = document.querySelector('.box-slider_img1');
-  var catActive = document.querySelector('.cat');
-  var numberActive = document.querySelector('.box-number');
-  var buttonDisplay = document.querySelector('.button');
-  var rhombus1 = document.querySelector('.rhombus:nth-child(1)');
-  var rhombus2 = document.querySelector('.rhombus:nth-child(2)');
-  var rhombusImg = document.querySelector('.rhombus-img');
-  var mailActive = document.querySelector('.mail');
 
-  boxFlower.classList.add("active");
-  boxFlower2.classList.add("active");
-  boxFlower3.classList.add("active");
-  boxFlower4.classList.add("active");
-  boxFlower5.classList.add("active");
-  boxFlower6.classList.add("active");
-  circleActive.classList.add("active");
-  boxsliderimg1.classList.add("active");
-  catActive.classList.add("active");
-  numberActive.classList.add("active");
-  buttonDisplay.classList.add("active");
-  rhombus1.classList.add("active");
-  rhombus2.classList.add("active");
-  rhombusImg.classList.add("active");
-  mailActive.classList.add("active");
+// ------audio------------
+var buttonSong = document.querySelector('.button')
+var mySong = document.getElementById("song")
+buttonSong.onclick= function(){
+    if(mySong.paused){
+        mySong.play()
+    }
+}
+function isMobile() {
+  return /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+}
 
-  var mySong = document.getElementById("song");
-  if(mySong && mySong.paused){
-    mySong.play();
+function checkOrientation() {
+  const warning = document.getElementById("rotate-warning");
+
+  if (!isMobile()) {
+    warning.style.display = "none";
+    return;
+  }
+
+  if (window.innerWidth < window.innerHeight) {
+    // đang dọc
+    warning.style.display = "flex";
+  } else {
+    // đã ngang
+    warning.style.display = "none";
+    requestFullscreen();
   }
 }
 
-// ❌ XÓA đoạn tự gọi animation bằng nút trong animation
-// buttonActive.onclick = function(){
-//   startAnimation();
-// }
-
-
-// mail popup
-var mail = document.querySelector(".mail");
-var slider3 = document.querySelector(".slider3");
-var closeSlider3 = document.querySelector(".fa-xmark");
-
-if(mail){
-  mail.onclick = function(){
-    slider3.classList.add("active");
+function requestFullscreen() {
+  const elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
   }
 }
 
-if(closeSlider3){
-  closeSlider3.addEventListener('click', function(){
-    slider3.classList.remove('active');
-  });
-}
+window.addEventListener("resize", checkOrientation);
+window.addEventListener("load", checkOrientation);
